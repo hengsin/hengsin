937b31de2d669fa8b33bd42a824a9b96a3ed60d6
hengsin <hengsin@gmail.com>
2022-09-24 05:17:58 +0800

    add checking of IDEMPIERE_BUILD variable. get plugin list from repository/plugins folder which is more reliable

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 echo "Install plugins in repository to target/products/org.adempiere.server.product"
 
+set -u
+: "$IDEMPIERE_BUILD"
+
 mkdir -p target
 cd target
 rm -r -f *
 cp -r $IDEMPIERE_BUILD/org.idempiere.p2/target/products .
 cd ..
 
-cd plugins
+cd repository/plugins
 PLUGINS=''
 for filename in *.jar;do
     PLUGINS=$PLUGINS,"${filename%_*}"
@@ -14,7 +17,7 @@ done
 PLUGINS="${PLUGINS:1}"
 export plugins=$PLUGINS
 
-cd ..
+cd ../..
 
 export osgi_os=linux
 export osgi_ws=gtk
@@ -28,3 +31,7 @@ mvn -P installPlugins
 export osgi_os=win32
 export osgi_ws=win32
 mvn -P installPlugins
+
+cp sysconfig.properties  target/products/org.adempiere.server.product/linux/gtk/x86_64
+cp sysconfig.properties  target/products/org.adempiere.server.product/macosx/cocoa/x86_64
+cp sysconfig.properties  target/products/org.adempiere.server.product/win32/win32/x86_64
